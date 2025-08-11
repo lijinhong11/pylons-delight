bf82c6410fc858aa996920aa9466238080f355c2
lijinhong11 <62691956+lijinhong11@users.noreply.github.com>
2025-08-11 21:27:36 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    java
+    idea
+    id("com.gradleup.shadow") version "8.3.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("io.freefair.lombok") version "8.13.1"
+}
+
+group = project.properties["group"]!!
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc"
+    }
+    maven("https://repo.xenondevs.xyz/releases")
+}
+
+val coreVersion = project.properties["pylon-core.version"] as String
+val baseVersion = project.properties["pylon-base.version"] as String
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.github.pylonmc:pylon-core:$coreVersion")
+    compileOnly("io.github.pylonmc:pylon-base:$baseVersion")
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.shadowJar {
+    mergeServiceFiles()
+
+    fun doRelocate(lib: String) {
+        relocate(lib, "$group.shadowlibs.$lib")
+    }
+
+    archiveBaseName = project.name
+    archiveClassifier = null
+}
+
+bukkit {
+    name = project.properties["name"] as String
+    main = project.properties["main-class"] as String
+    version = project.version.toString()
+    apiVersion = "1.21"
+    depend = listOf("PylonCore")
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+}
+
+tasks.runServer {
+    downloadPlugins {
+        github("pylonmc", "pylon-core", coreVersion, "pylon-core-$coreVersion.jar")
+    }
+    downloadPlugins {
+        github("pylonmc", "pylon-base", coreVersion, "pylon-base-$coreVersion.jar")
+    }
+    maxHeapSize = "4G"
+    minecraftVersion("1.21.4")
+}
